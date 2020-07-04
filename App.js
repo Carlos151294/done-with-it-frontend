fbf054248219dee12281504bfbd5c65fe543255c
Carlos Flores <gbs@MacBook-Pro-de-Gabriela.local>
2020-07-04 10:40:32 -0500

    Components names updated

--- App.js
+++ App.js
@@ -2,10 +2,18 @@ import React from 'react';
 import { StyleSheet } from 'react-native';
 
 import ListingEditScreen from './app/screens/ListingEditScreen';
+import AccountScreen from './app/screens/AccountScreen';
+import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
+import ListingsScreen from './app/screens/ListingsScreen';
+import LoginScreen from './app/screens/LoginScreen';
+import MessagesScreen from './app/screens/MessagesScreen';
+import RegisterScreen from './app/screens/RegisterScreen';
+import { ViewImageScreen } from './app/screens/ViewImageScreen';
+import { WelcomeScreen } from './app/screens/WelcomeScreen';
 
 export default function App() {
   return (
-    <ListingEditScreen />
+    <RegisterScreen />
   );
 }
 
