8091f9087b594be656392068197608ba96769170
Carlos Flores <gbs@MacBook-Pro-de-Gabriela.local>
2020-07-03 21:56:53 -0500

    Useless array deleted

--- App.js
+++ App.js
@@ -3,12 +3,6 @@ import { StyleSheet } from 'react-native';
 
 import ListingEditScreen from './app/screens/ListingEditScreen';
 
-// const categories = [
-//   { label: "Forniture", value: 1 },
-//   { label: "Clothing", value: 2 },
-//   { label: "Cameras", value: 3 },
-// ];
-
 export default function App() {
   return (
     <ListingEditScreen />
