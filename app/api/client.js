e021fc50060330077f580bd80ef00a207df994f0
Carlos Flores Nava <carlos.flores.nava@hotmail.com>
2021-01-05 19:34:38 -0600

    Cache layer added. Get requests cached.

--- app/api/client.js
+++ app/api/client.js
@@ -1,8 +1,22 @@
 import { create } from 'apisauce';
+import cache from '../utility/cache';
 
 // Define the api
 const apiClient = create({
    baseURL: 'http://localhost:9000/api'
  });
+
+ const get = apiClient.get;
+ apiClient.get = async (url, params, axiosConfig) => {
+  const response = await get(url, params, axiosConfig);
+  if (response.ok) {
+    cache.store(url, response.data);
+    return response;
+  }
+
+  // If server response fails or there's no internet conection
+  const data = await cache.get(url);
+  return data ? { ok: true, data } : response;
+};
 
  export default apiClient;
